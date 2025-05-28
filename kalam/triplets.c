4c5132fec249416cc89bd62e8e7a4ee862374923
Kalaam <29440904+kali-1997@users.noreply.github.com>
2025-05-28 16:18:57 +0530

    Add files via upload

---
+++ kalam/triplets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+for(int a=0;a<1000;a++ )
+{
+    for(b=a+1;b<1000;b++)
+    {
+        for(c=b+1;c<1000;c++)
+        if(((a*a +b*b == c*c) && ((a+b+c)==1000)))
+        printf("%da","%db", "%dc",a,b,c);
+    }
+}
+}
