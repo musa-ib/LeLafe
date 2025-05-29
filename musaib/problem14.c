5627fc86d5842f5165cdc3277cb7acbbc3414ef0
musa-ib <81172749+musa-ib@users.noreply.github.com>
2025-05-29 14:27:31 +0530

    Add files via upload

---
+++ musaib/problem14.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+
+int seq_len [1000000] = {0};
+
+int count_seq(long long n){
+    int c = 1;
+    long long  n1 = n; 
+    while(n>1){
+        if(n<n1 && seq_len[n]!=0){
+            c = c+seq_len[n]-1;
+            return c;
+        }
+        else if(n%2==0){
+            n = n/2;
+            c++;
+        }
+        else{
+            n = 3*n+1;
+            c++;
+        }
+    }
+    return c;
+}
+int main(){
+    long long number,n = 1000000;
+    // int n = 10;
+    int cc = 0;
+    int c;
+    for(long long i=1;i<=n;i++){
+        c = count_seq(i);
+        seq_len[i] = c;
+        if(c>cc){
+            cc=c;
+            number = i;  
+        }
+    }
+    printf("%d\t%d",cc,number);
+}
