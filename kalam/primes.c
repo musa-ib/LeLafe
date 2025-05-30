6590fa60b29991dc314d0f21d5f9fe754c7dc35e
Kalaam <29440904+kali-1997@users.noreply.github.com>
2025-05-30 16:52:50 +0530

    Update primes.c

--- kalam/primes.c
+++ kalam/primes.c
@@ -5,6 +5,11 @@
 #define LIMIT 2000000
 
 int main() {
+
+clock_t start, end;
+double cpu_time_used;
+start = clock(); 
+    
     bool sieve[LIMIT + 1];
     for (int i = 0; i <= LIMIT; i++) {
         sieve[i] = true;
@@ -30,5 +35,11 @@ int main() {
     }
 
     printf("Sum of all primes up to %d is: %llu\n", LIMIT, sum);
+    
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", cpu_time_used);
+
+    
     return 0;
 }
